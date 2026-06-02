f2fc1db18aace9b844a96b36c34d76a316dff6a9
dando <danila.dor.1125@mail.ru>
2026-06-02 23:54:07 +0500

    FormatLastMessage.lib DONE

--- Client/main.cpp
+++ Client/main.cpp
@@ -7,12 +7,12 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <iphlpapi.h>
+#include <FormatLastError.h>
 
 #pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "FormatLastError.lib")
 using namespace std;
 #define MTU 1500
-
-CHAR* FormatLastError(DWORD dwError, CHAR szError[]);
 
 void main()
 {
@@ -102,21 +102,3 @@ void main()
 	WSACleanup();
 }
 
-CHAR* FormatLastError(DWORD dwError, CHAR szError[])
-{
-	LPSTR lpError = NULL;
-	FormatMessage
-	(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dwError,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPSTR)&lpError,
-		0,
-		NULL
-	);
-	//strcpy(szError, lpError);
-	sprintf(szError, "Error %i:%s", dwError, lpError);
-	LocalFree(lpError);
-	return szError;
-}
