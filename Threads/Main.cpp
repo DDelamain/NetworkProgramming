7cdc07b1e89436ab1ceb3d47e980c735033f1576
dando <danila.dor.1125@mail.ru>
2026-06-13 23:35:22 +0500

    Threads

--- Threads/Main.cpp
+++ Threads/Main.cpp
@@ -1,18 +1,64 @@
 #include <iostream>
 #include <Windows.h>
-using namespace std;
+#include<thread>
+#include<chrono>
+using std::cin;
+using std::cout;
+using std::endl;
+using namespace std::chrono_literals;
+
+bool finish = false;
 
 VOID Function()
 {
-	while (true)
+	while (!finish)
 	{
 		cout << "Hello Threads" << GetCurrentThreadId() << endl;
 		system("PAUSE");
 	}
 }
+struct Point
+{
+	Point(int x,int y):x(x),y(y){}
+	int x;
+	int y;
+};
+VOID Collision(Point* point)
+{
+	while (point->x != point->y)
+	{
+		cout << "X= " << point->x++ << "\tY = " << point->y-- << endl;
+		Sleep(10);
+	}
+}
+VOID Decrement(int i)
+{
+	while (i)cout << i-- << "\t";
+}
+void Plus()
+{
+	while (!finish)
+	{
+		cout << "+";
+		std::this_thread::sleep_for(100ms);
+	}
+	//std::this_thread::
+}
+void Minus()
+{
+	while (!finish)
+	{
+		cout << "-";
+		std::this_thread::sleep_for(100ms);
+	}
+}
+//#define WINDOWS_THREADS_1
+//#define WINDOWS_THREADS_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef WINDOWS_THREADS_1
 	DWORD dwID = 0;
 	HANDLE hThread = CreateThread
 	(
@@ -23,5 +69,38 @@ void main()
 		NULL,
 		&dwID
 	);
-	WaitForSingleObject(hThread,INFINITE);
+	cin.get();
+	finish = true;
+	cout << "ThreadID from main()" << dwID << endl;
+	WaitForSingleObject(hThread, INFINITE);
+#endif // WINDOWS_THREADS_1
+
+#ifdef WINDOWS_THREADS_2
+	Point A(0, 10000);
+	int i = 10000;
+	DWORD dwThreadID = 0;
+	HANDLE hThread = CreateThread
+	(
+		NULL,
+		NULL,
+		(LPTHREAD_START_ROUTINE)Decrement,
+		(LPVOID)i,
+		NULL,
+		&dwThreadID
+	);
+	WaitForSingleObject(hThread, INFINITE);
+#endif // WINDOWS_THREADS_2
+
+	//Plus();
+	//Minus();
+
+	std::thread plus_thread = std::thread(Plus);
+	std::thread minus_thread = std::thread(Minus);
+	cout << "start\n";
+	cin.get();
+	finish = true;
+	cout << "\nfinish";
+
+	if(plus_thread.joinable())plus_thread.join();
+	if (minus_thread.joinable())minus_thread.join();
 }
